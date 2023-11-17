69802d709a599a349fdb7c02b15bf260b9f96e1f
bernimontero <bmonteromorizzio@udesa.edu.ar>
2023-11-17 17:21:11 -0300

    agregando cosas

--- js/detalle-peli.js
+++ js/detalle-peli.js
@@ -49,7 +49,7 @@ boton.addEventListener("click", function (e) {
         let contenido= "";
         for (let index = 0; index < 5; index++) {
             contenido+= `<article class="cajaHija">
-                <a href="./detalle-peli.html?id=${data.results[index].id}"><img class="peliculas" src="https://image.tmdb.org/t/p/w500/${data.results[index].poster_path}" alt="${data.results[index].title}"></a>
+                <a href="./detalle-peli.html?id=${data.results[index].id}&name=${data.genres[index].name}"><img class="peliculas" src="https://image.tmdb.org/t/p/w500/${data.results[index].poster_path}" alt="${data.results[index].title}"></a>
                 <h3 class="tituloPelicula"><strong>${data.results[index].title}</strong></h3><h4 class="tituloPelicula">${data.results[index].release_date}</h4>
             </article>`
             
