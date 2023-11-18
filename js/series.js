05fc294cb89c9a285685e3e2760b34bd6240a4d5
victoriammaldonado <maldonadom@udesa.edu.ar>
2023-11-17 23:34:46 -0300

    ...solucionando errores

--- js/series.js
+++ js/series.js
@@ -27,8 +27,8 @@ boton.addEventListener("click", function (e) {
         let contenido= "";
         for (let index = 0; index < 5; index++) {
             contenido+= `<article class="cajaHija">
-                <a href="./detalle-peli.html?id=${data.results[index].id}&name=${data.genres[index].name}"><img class="peliculas" src="https://image.tmdb.org/t/p/w500/${data.results[index].poster_path}" alt="${data.results[index].title}"></a>
-                <h3 class="tituloPelicula"><strong>${data.results[index].title}</strong></h3><h4 class="tituloPelicula">${data.results[index].release_date}</h4>
+                <a href="./series.html?id=${data.results[index].id}"><img class="peliculas" src="https://image.tmdb.org/t/p/w500/${data.results[index].poster_path}" alt="${data.results[index].name}"></a>
+                <h3 class="tituloPelicula"><strong>${data.results[index].name}</strong></h3><h4 class="tituloPelicula">${data.results[index].first_air_date}</h4>
             </article>`
             
         }
